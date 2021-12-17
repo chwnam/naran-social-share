def8fe4193e7c19bc31f4d60a74777fd6d3bcca2
changwoo <ep6tri@hotmail.com>
2021-12-17 14:17:07 +0900

    Update version to 0.4.2

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -52,7 +52,6 @@ return array(
     'NSS_Register_Script' => $baseDir . '/includes/registers/class-nss-register-script.php',
     'NSS_Register_Shortcode' => $baseDir . '/includes/registers/class-nss-register-shortcode.php',
     'NSS_Register_Style' => $baseDir . '/includes/registers/class-nss-register-style.php',
-    'NSS_Register_Uninstall' => $baseDir . '/includes/registers/class-nss-register-uninstall.php',
     'NSS_Registers' => $baseDir . '/includes/modules/class-nss-registers.php',
     'NSS_Script_Helper' => $baseDir . '/core/etc/class-nss-script-helper.php',
     'NSS_Setup' => $baseDir . '/includes/modules/class-nss-setup.php',
