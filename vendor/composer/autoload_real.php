5665ecae610dd8a34dde05084b54629110633799
changwoo <ep6tri@hotmail.com>
2022-01-20 14:15:26 +0900

    Update autoloading files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -56,11 +56,16 @@ class ComposerAutoloaderInit0cbadd31a026f692ed656e4ec145971e
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequire0cbadd31a026f692ed656e4ec145971e($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
